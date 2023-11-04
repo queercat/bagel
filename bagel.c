f0235529e6d0dfecbad9f6e06ecfa1176528f670
May Tusek <tusek.may@gmail.com>
2023-11-04 13:54:44 -0700

    Adds boiler REPL and "build" script.

---
+++ bagel.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void bgl_error(char* message) {
+    printf("%s", message);
+    exit(1);
+}
+
+void _strip_newlines(char** buffer) {
+    (*buffer)[strcspn(*buffer, "\n")] = 0;
+}
+
+char* bgl_read() {
+    int size = 1024;
+    char* line = malloc(sizeof(char) * size);
+    printf("> ");
+
+    fgets(line, size, stdin);
+    _strip_newlines(&line);
+
+    return line;
+}
+
+char* bgl_eval(char* text) {
+    return text;
+}
+
+void bgl_print(char* text) {
+    printf("%s\n", text);
+    free(text);
+}
+
+int main() {
+    for (;;) {
+        bgl_print(bgl_eval(bgl_read()));
+    }
+}
---
+++ build.ps1
@@ -0,0 +1,6 @@
+# Powershell
+# Build script for the project
+
+tcc bagel.c -o bagel.exe
+./bagel.exe
+rm bagel.exe
